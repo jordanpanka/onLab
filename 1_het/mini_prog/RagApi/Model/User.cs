742d5aa3f48076730dc7a5440e957ce31ff5e4b2
jordanpanka <jordanpankaboglarka@gmail.com>
2026-03-01 13:15:36 +0100

    stop program

--- 1_het/mini_prog/RagApi/Model/User.cs
+++
@@ -1,8 +0,0 @@
-public class User
-{
-    public int ID {get; set;}
-    public string Email {get; set;}
-    public byte[] PasswordHash {get; set;}
-    public byte[] Passwordsalt {get; set;}
-    public DateTime CreatedTime {get; set;}
-}
--- 1_het/mini_prog/RagApi/obj/Debug/net9.0/RagApi.AssemblyInfo.cs
+++ 1_het/mini_prog/RagApi/obj/Debug/net9.0/RagApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RagApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b850fbc6407c970990e88dff4dde8d85629b6c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2c205304ee4050ad329f11fef672e23ca21d0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RagApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RagApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
