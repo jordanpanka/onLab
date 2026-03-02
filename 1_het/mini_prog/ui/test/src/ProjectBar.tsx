1aac692131ca659c004c6c2262f622441ab86388
jordanpanka <jordanpankaboglarka@gmail.com>
2026-03-02 15:08:28 +0100

    Sidebar layout

--- 1_het/mini_prog/ui/test/src/ProjectBar.tsx
+++ 1_het/mini_prog/ui/test/src/ProjectBar.tsx
@@ -1,5 +1,6 @@
 import { Box, IconButton, InputAdornment, List, ListItemButton, ListItemText, TextField, Typography } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
+import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
 import SearchIcon from "@mui/icons-material/Search";
 
 import { useEffect, useState } from "preact/hooks"
@@ -8,10 +9,11 @@ type Project = {
     name: string,
     description: string
 }
+const openwidth = 240;
+const closedWidth = 60;
 export function ProjectBar() {
     const [search, setSearch] = useState("");
     const [newProject, setNewProject] = useState<Project>();
-    const [full, setFull] = useState(true);
     const [projects, setProjects] = useState<Project[]>([]);
     const [open, setOpen] = useState(true);
     async function onClick() {
@@ -33,19 +35,20 @@ export function ProjectBar() {
         const response = await fetch("api/projects/load");
         const data = await response.json();
         setProjects(data);
-
     }
     return (<>
-        <Drawer variant="persistent" anchor="left" open={open}>
-            <Typography sx={{
-                "& .MuiInputBase-input": {
+        <Drawer variant="persistent" anchor="left" open={open} sx={{ width: open ? openwidth : closedWidth, gap:2}}>
+            <Box sx={{ display: "flex", justifyContent: "space-between", height: 56, alignItems: "center", 
+                marginLeft:2, marginRight:2
+             }}>
+                <Typography sx={{
+                    lineHeight: 1,
                     fontFamily: "'Inter', sans-serif",
-                },
-                "& .MuiInputLabel-root": {
-                    fontFamily: "'Playfair Display', serif",
-                },
-            }}>My projects</Typography>
-            <Box sx={{ display: "flex" }}>
+                }}>My projects</Typography>
+                <IconButton><CreateNewFolderIcon /></IconButton>
+            </Box>
+
+            <Box sx={{ display: "flex", marginLeft:2, marginRight:2}}>
                 <TextField size="small" placeholder="Search..." InputProps={{
                     endAdornment: (<InputAdornment position="end"> <IconButton>
                         <SearchIcon />
